4e5753f75d9554b292e480070c63bdc6e187ca7e
Artur Vale <ArturllVale@github.com>
2024-12-16 12:43:42 -0300

    Aidionando menu de navegação

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,15 @@
 "use client";
-import { useState } from 'react';
+import Navbar from './components/navbar';
+import { useState, useEffect } from 'react';
 
 export default function Home() {
-  const [isDarkTheme, setIsDarkTheme] = useState(false);
-
-  const toggleTheme = () => {
-    setIsDarkTheme(!isDarkTheme);
-    document.body.classList.toggle('dark', !isDarkTheme);
-  };
-
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <button onClick={toggleTheme} className="absolute top-4 right-4 p-2 bg-gray-200 rounded-full dark:bg-gray-700">
-        {isDarkTheme ? '🌙' : '☀️'}
-      </button>
+    <div className="grid grid-rows-[20px_1fr_20px] min-h-screen pb-20 gap-16 font-[family-name:var(--font-geist-sans)]">
+      <Navbar />
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      <footer className="row-start-3 flex gap-1 flex-wrap items-center justify-center">
         <a
           href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           target="_blank"
